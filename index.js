8ae02622f758adfa7dd1f46ceaa923b51ec1f17c
josealbertoviuge-coder <josealbertoviuge@gmail.com>
2026-02-19 16:32:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 const app = express();
 app.use(express.json());
 
-const TOKEN = process.env.8294267757:AAG6F4Lp0ivlxZxWfp7O2xApWuFuyAyUBy0;
+const TOKEN = process.env.TELEGRAM_TOKEN;
 
 async function enviarMensagem(chatId, texto) {
   await fetch(`https://api.telegram.org/bot${8294267757:AAG6F4Lp0ivlxZxWfp7O2xApWuFuyAyUBy0}/sendMessage`, {
